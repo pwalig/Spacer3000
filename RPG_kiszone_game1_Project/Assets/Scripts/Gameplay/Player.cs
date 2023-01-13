cad3a4372899f38478112e1e1159576be2b51273
jankiw <jan.kiwlenko@gmail.com>
2023-01-13 21:14:11 +0100

    Player movement boundaries

--- RPG_kiszone_game1_Project/Assets/Scripts/Gameplay/Player.cs
+++ RPG_kiszone_game1_Project/Assets/Scripts/Gameplay/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Player : SpaceshipController
@@ -8,6 +9,15 @@ public class Player : SpaceshipController
     {
         moveDirectionX = Input.GetAxisRaw("Horizontal");
         moveDirectionY = Input.GetAxisRaw("Vertical");
+
+        if((transform.position.y>= 0 && moveDirectionY == 1) || (transform.position.y <= -70 && moveDirectionY == -1))
+        {
+            moveDirectionY = 0;
+        }
+        if ((transform.position.x >= 65 && moveDirectionX == 1) || (transform.position.x <= -65 && moveDirectionX == -1))
+        {
+            moveDirectionX = 0;
+        }
 
         shoot = Input.GetKey(KeyCode.Space);
     }
