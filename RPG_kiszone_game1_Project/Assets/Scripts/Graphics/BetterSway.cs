d1aed58acf801cea510fd05492d56bbd7673b634
Paweł <pwalig03@gmail.com>
2023-02-11 19:24:44 +0100

    Add physics based sway system

---
+++ RPG_kiszone_game1_Project/Assets/Scripts/Graphics/BetterSway.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterSway : MonoBehaviour
+{
+    SpaceshipController controller;
+    [SerializeField] List<SecondOrderDynamics> systems = new List<SecondOrderDynamics>();
+    void Awake()
+    {
+        controller = GetComponentInParent<SpaceshipController>(); //get controller
+        if (controller == null) controller = gameObject.AddComponent<SpaceshipController>(); //if controller is missing create empty one
+        foreach (SecondOrderDynamics system in systems)
+        {
+            system.Initialise();
+        }
+    }
+
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(systems[1].Update(controller.moveDirectionY), systems[0].Update(-controller.moveDirectionX), 0f);
+    }
+}
---
+++ RPG_kiszone_game1_Project/Assets/Scripts/Graphics/SecondOrderDynamics.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SecondOrderDynamics
+{
+    [SerializeField] float f = 1f; //system speed
+    [SerializeField] float z = 0.5f; //damping
+    [SerializeField] float r = 0f; //initial response
+    [SerializeField] float amplitude = 1f; //output input ratio
+    [SerializeField] float offset = 0f;
+    float xp;
+    float y, yd;
+    float k1, k2, k3;
+    public void Initialise (float x0 = 0)
+    {
+        //compute constants
+        k1 = z / (Mathf.PI * f);
+        k2 = 1 / (4 * Mathf.PI * Mathf.PI * f * f);
+        k3 = r * z / (2 * Mathf.PI * f);
+
+        xp = x0;
+        y = x0;
+        yd = 0;
+    }
+
+    ///<summary>
+    ///Computes physics based output interpolation
+    ///</summary>
+    public float Update (float x)
+    {
+        float xd = (x - xp) * Time.deltaTime; //calculate input acceleration
+        xp = x; //update input position
+        float k2_stable = Mathf.Max(k2, 1.1f * (Time.deltaTime * Time.deltaTime / 4 + Time.deltaTime * k1 / 2));
+        y += Time.deltaTime * yd; //update output position
+        yd += Time.deltaTime * (x + k3 * xd - y - k1 * yd) / k2_stable; //update output acceleration
+        return y * amplitude + offset;
+    }
+}
