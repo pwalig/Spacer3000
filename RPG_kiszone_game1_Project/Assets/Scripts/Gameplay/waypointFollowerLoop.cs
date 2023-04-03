6599d636d642e922e372d67f262f72e2ee6daee9
jankiw <jan.kiwlenko@gmail.com>
2023-04-04 01:39:37 +0200

    waypoints

---
+++ RPG_kiszone_game1_Project/Assets/Scripts/Gameplay/waypointFollowerLoop.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class waypointFollowerLoop : SpaceshipController
+{
+    public GameObject waypoints;
+    public int counter;
+
+    void Start()
+    {
+        counter = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(this.transform.position, waypoints.GetComponent<waypoints>().wp[counter].transform.position) < 0.2f)
+        {
+            counter++;
+            counter %= waypoints.GetComponent<waypoints>().wp.Length;
+        }
+        Vector3 moveDirection = new Vector3(waypoints.GetComponent<waypoints>().wp[counter].transform.position.x - transform.position.x, waypoints.GetComponent<waypoints>().wp[counter].transform.position.y - transform.position.y, 0);
+        if (moveDirection.magnitude > 1f) moveDirection.Normalize();
+        moveDirectionX = moveDirection.x;
+        moveDirectionY = moveDirection.y;
+    }
+}
---
+++ RPG_kiszone_game1_Project/Assets/Scripts/Gameplay/waypoints.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypoints : MonoBehaviour
+{
+    public GameObject[] wp;
+    // Start is called before the first frame update
+
+}
