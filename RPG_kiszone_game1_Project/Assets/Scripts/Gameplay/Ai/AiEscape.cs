3e094a1b099337d639fa125b45716a9e74990c60
Paweł <pwalig03@gmail.com>
2023-04-15 00:53:34 +0200

    AiEscape bug fixes

--- RPG_kiszone_game1_Project/Assets/Scripts/Gameplay/Ai/AiEscape.cs
+++ RPG_kiszone_game1_Project/Assets/Scripts/Gameplay/Ai/AiEscape.cs
@@ -8,13 +8,13 @@ public class AiEscape : SpaceshipController
 
     public IEnumerator FlyAway(float delay=0f)
     {
-        escape = true;
         yield return new WaitForSeconds(delay);
+        escape = true;
         Vector2 dir = new Vector2(moveDirectionX, moveDirectionY).normalized;
         moveDirectionX = dir.x; moveDirectionY = dir.y;
         if (dir.magnitude < 0.1f)
             moveDirectionX = 1f;
-        yield return new WaitForSeconds(20f);
+        while(Mathf.Abs(transform.position.x) < GameplayManager.gameAreaSize.x + 40f && Mathf.Abs(transform.position.y) < GameplayManager.gameAreaSize.y + 40f) yield return 0;
         LevelManager.enemies.Remove(gameObject);
         Destroy(gameObject);
     }
--- RPG_kiszone_game1_Project/Assets/Scripts/Gameplay/Ai/waypointFollowerLoop.cs
+++ RPG_kiszone_game1_Project/Assets/Scripts/Gameplay/Ai/waypointFollowerLoop.cs
@@ -4,7 +4,7 @@ using System.Collections.Specialized;
 using System.Security.Cryptography;
 using UnityEngine;
 
-public class waypointFollowerLoop : SpaceshipController
+public class waypointFollowerLoop : AiEscape
 {
     public GameObject waypoints;
     public int counter;
@@ -16,14 +16,17 @@ public class waypointFollowerLoop : SpaceshipController
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(this.transform.position, waypoints.GetComponent<waypoints>().wp[counter].transform.position) < 0.2f)
+        if(!escape)
         {
-            counter++;
-            counter %= waypoints.GetComponent<waypoints>().wp.Length;
+            if(Vector3.Distance(this.transform.position, waypoints.GetComponent<waypoints>().wp[counter].transform.position) < 0.2f)
+            {
+                counter++;
+                counter %= waypoints.GetComponent<waypoints>().wp.Length;
+            }
+            Vector3 moveDirection = new Vector3(waypoints.GetComponent<waypoints>().wp[counter].transform.position.x - transform.position.x, waypoints.GetComponent<waypoints>().wp[counter].transform.position.y - transform.position.y, 0);
+            if (moveDirection.magnitude > 1f) moveDirection.Normalize();
+            moveDirectionX = moveDirection.x;
+            moveDirectionY = moveDirection.y;
         }
-        Vector3 moveDirection = new Vector3(waypoints.GetComponent<waypoints>().wp[counter].transform.position.x - transform.position.x, waypoints.GetComponent<waypoints>().wp[counter].transform.position.y - transform.position.y, 0);
-        if (moveDirection.magnitude > 1f) moveDirection.Normalize();
-        moveDirectionX = moveDirection.x;
-        moveDirectionY = moveDirection.y;
     }
 }
